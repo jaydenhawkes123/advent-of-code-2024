8e71c76c68788b3dee134aecd545e3bbdea40434
jayden <jayden@jaydenhawkes.com>
2024-12-06 22:13:05 +1100

    Fill the lists with the locations

--- main.go
+++ main.go
@@ -2,21 +2,38 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
 	data, err := os.Open("locations.txt")
 	if err != nil {
 		log.Printf("Error reading file: %s", err)
+		return
 	}
 
 	fileScanner := bufio.NewScanner(data)
 	fileScanner.Split(bufio.ScanWords)
-
+	var listA []int
+	var listB []int
+	flip := true
 	for fileScanner.Scan() {
-		fmt.Printf("Location %s \n", fileScanner.Text())
+		locationStr := fileScanner.Text()
+		location, err := strconv.Atoi(locationStr)
+
+		if err != nil {
+			log.Printf("Error parsing location id: %s", err)
+			return
+		}
+
+		if flip {
+			flip = !flip
+			listA = append(listA, location)
+			continue
+		}
+		flip = !flip
+		listB = append(listB, location)
 	}
 }
